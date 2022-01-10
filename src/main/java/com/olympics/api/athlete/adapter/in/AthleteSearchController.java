285208ec4886067f9a4d01deda362459e0345ca2
pantsousis <pantsousis@gmail.com>
2022-01-10 17:45:25 +0200

    Added CrossOrigin Annotation

--- src/main/java/com/olympics/api/athlete/adapter/in/AthleteSearchController.java
+++ src/main/java/com/olympics/api/athlete/adapter/in/AthleteSearchController.java
@@ -1,9 +1,9 @@
 package com.olympics.api.athlete.adapter.in;
 
 import com.olympics.api.athlete.application.AthleteSearchService;
-import com.olympics.api.athlete.domain.Athlete;
 import com.olympics.entity.AthleteEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,8 @@ public class AthleteSearchController {
     private final AthleteSearchService athleteSearchService;
 
     @GetMapping("/athlete/{team}")
-    public List<AthleteEntity> getAthletesByTeam(@PathVariable(name = "team") String team){
+    @CrossOrigin("http://localhost:3000")
+    public List<AthleteEntity> getAthletesByTeam(@PathVariable(name = "team") String team) {
         return athleteSearchService.getAthletesByTeam(team);
     }
 }
